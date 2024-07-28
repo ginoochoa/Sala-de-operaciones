54356893db407c16a7ffa2b25337964df203f4c7
ginoochoa <93208768+ginoochoa@users.noreply.github.com>
2024-07-28 11:42:10 -0500

    Delete operating_room_reservations directory

--- operating_room_reservations/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import operating_room
-from . import reservation
-from . import expense
-from . import hospitalization
--- operating_room_reservations/models/operating_room.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class OperatingRoom(models.Model):
-    _name = 'operating.room'
-    _description = 'Sala de operaciones'
-
-    name = fields.Char(string='Nombre')
-    capacity = fields.Integer(string='Capacidad')
